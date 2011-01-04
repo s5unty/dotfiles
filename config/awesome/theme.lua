f3cb98ffe6b5bae6352d4365e43fc9a89763cad8
Vern Sun <s5unty@gmail.com>
2011-01-04 14:09:18 +0800

    1294121358

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -16,7 +16,7 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.border_width  = "0"
+theme.border_width  = "1"
 theme.border_normal = "#222222"
 theme.border_focus  = "#666622"
 theme.border_marked = "#91231c"
