c0db7f149e6af6104a663f2e8c778af571f4ac18
Vern Sun <s5unty@gmail.com>
2012-09-19 14:34:25 +0800

    1348036465

--- todo/actions/todo_ff.sh
+++ todo/actions/todo_ff.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-export COLOR_PRIORITY="$RED"
-export COLOR_CONTEXT="$GREEN"
-export COLOR_PROJECT="$CYAN"
-export COLOR_ADD_ONS="$PURPLE"
-export COLOR_REMINDS="$WHITE"
+export COLOR_CONTEXT="$AT_UNDERL"
+export COLOR_PROJECT="$AT_BOLD"
+export COLOR_ADD_ONS="$AT_ITALIC"
 
 # Force gawk to behave posixly. Comment out if you get an error about
 # no such option -W.
@@ -36,12 +34,11 @@ if [ "$TODOTXT_PLAIN" -eq "0" ]; then
         /\(A\)/ { color = "PRI_A" }
         /\(B\)/ { color = "PRI_B" }
         /\(C\)/ { color = "PRI_C" }
-        /\([D-Z]\)/ { color = "PRI_X" }
-        / x / { color = "COLOR_DONE" }
+        /\(I\)/ { color = "PRI_I" }
+        /\(F\)/ { color = "PRI_F" }
         {
-            $0=gensub(/ ([A-Z]) /,              " " highlight("COLOR_PRIORITY") "\\1" highlight(color), "g", $0);
             $0=gensub(/ (@\w*)/,                " " highlight("COLOR_CONTEXT")  "\\1" highlight(color), "g", $0);
-            $0=gensub(/ (\+[[:alnum:]_\-/]*)/,   " " highlight("COLOR_PROJECT")  "\\1" highlight(color), "g", $0);
+            $0=gensub(/ (\+[[:alnum:]_\-/]*)/,  " " highlight("COLOR_PROJECT")  "\\1" highlight(color), "g", $0);
             $0=gensub(/ ([[:alnum:]_\-/@#]+:[[:alnum:]=.<>_\-/@#]+)/,
                                                 " " highlight("COLOR_ADD_ONS")  "\\1" highlight(color), "g", $0);
             print
