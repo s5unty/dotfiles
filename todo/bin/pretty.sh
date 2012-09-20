895a235e4e5984229b69ab265cf61818ded26465
Vern Sun <s5unty@gmail.com>
2012-09-20 16:34:09 +0800

    1348130049

--- todo/bin/pretty.sh
+++ todo/bin/pretty.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-export COLOR_CONTEXT="$AT_UNDERL"
-export COLOR_PROJECT="$AT_BOLD"
-export COLOR_ADD_ONS="$AT_ITALIC"
+export COLOR_CONTEXT="$AT_ITALIC"
+export COLOR_PROJECT="$AT_ITALIC"
+export COLOR_ADD_ONS="$AT_UNDERL"
 
 # Force gawk to behave posixly. Comment out if you get an error about
 # no such option -W.
 AWK_OPTIONS=""
 if [ "$TODOTXT_PLAIN" -eq "0" ]; then
     # 1. gawk 格式化 REM 开头的内容 `man remind`，添加 (*) 标记
-    # 2. eval 对添加了 (*) 标记的内容重新排序
+    #         格式化 x 开头的 done，添加 (x) 标记
+    # 2. eval 对添加了 (*) 和 (x) 标记的内容重新排序
     # 3. gawk 高亮关键字
     gawk $AWK_OPTIONS '
-        function highlight(colorVar, color) {
-            color = ENVIRON[colorVar]
-            gsub(/\\+033/, "\033", color)
-            return color
-        }
         {
             IGNORECASE = 1
-            $0=gensub(/ REM.*MSG[ ]+/, " (*) " highlight(color), "g", $0);
+            $0=gensub(/ REM.*MSG[ ]+/, " (*) ", "g", $0);
+            $0=gensub(/ x /, " (X) ", "g", $0);
             print
         }
     ' \
@@ -36,6 +33,7 @@ if [ "$TODOTXT_PLAIN" -eq "0" ]; then
         /\(C\)/ { color = "PRI_C" }
         /\(I\)/ { color = "PRI_I" }
         /\(F\)/ { color = "PRI_F" }
+        /\(X\)/ { color = "COLOR_DONE" }
         {
             $0=gensub(/ (@\w*)/,                " " highlight("COLOR_CONTEXT")  "\\1" highlight(color), "g", $0);
             $0=gensub(/ (\+[[:alnum:]_\-/]*)/,  " " highlight("COLOR_PROJECT")  "\\1" highlight(color), "g", $0);
