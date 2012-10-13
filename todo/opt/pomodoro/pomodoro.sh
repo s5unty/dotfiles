0d16c1c884db32214a8050ce4310a452124f860f
Vern Sun <s5unty@gmail.com>
2012-10-14 01:21:57 +0800

    1350148917

---
+++ todo/opt/pomodoro/pomodoro.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# inspired from http://feedelli.org/2012/07/29/bash-command-line-pomodoro-timer.html
+# punch-time-tracking: http://code.google.com/p/punch-time-tracking/
+
+# 每个闹钟就固定为 25 分钟，不允许任意设定时间。
+# 配合 Punch.py 才能记录钟的使用情况，方便统计。
+doing=$1
+limit=$((25*60*1000))
+step=$((5*60*1000))
+#limit=$((25*1000)) # 25秒测试用
+#step=$((5*1000))
+
+#### 垂直递增
+# while [ $limit -gt 0 ]; do
+#     notify-send -u critical -i appointment -t $limit "●"
+#     sleep $(($step / 1000))
+#     limit=$(($limit - $step))
+# done
+
+#### 垂直递减
+expire=$limit
+while [ $limit -gt 0 ]; do
+    notify-send -u critical -i appointment -t $limit "●"
+    limit=$(($limit - $step))
+done
+sleep $(($expire / 1000))
+
+#### 水平递增
+# notify-send -u critical -i appointment -t $step "●"
+# sleep $(($step / 1000))
+# notify-send -u critical -i appointment -t $step "●●"
+# sleep $(($step / 1000))
+# notify-send -u critical -i appointment -t $step "●●●"
+# sleep $(($step / 1000))
+# notify-send -u critical -i appointment -t $step "●●●●"
+# sleep $(($step / 1000))
+# notify-send -u critical -i appointment -t $step "●●●●●"
+# sleep $(($step / 1000))
+
+### 水平递减
+# 略 :)
+
+# 工作时间结束
+notify-send -u critical -i appointment -t $step "Please stop this work and take short break. ☻" "$doing"
+
+# 结束 Punch.py 里面的计时，强迫自己手工重设每个工作闹钟
+python $TODO_DIR/opt/punch/Punch.py out > /dev/null 2>&1
+
--- vim/ftplugin/markdown.vim
+++ vim/ftplugin/markdown.vim
@@ -1,5 +1,5 @@
 nmap <F8> :w ~/hacking/jekyll/<C-R>=strftime('%F')<CR>-<C-R>=tolower(substitute(strpart(getline(3), 7), " ", "_", "g"))<CR>.mkd
-nmap <F9> :!cd ~/hacking/jekyll; git add <C-R>=strftime('%F')<CR>-<C-R>=tolower(substitute(strpart(getline(3), 7), " ", "_", "g"))<CR>.mkd; git ci -m "+ <C-R>=tolower(strpart(getline(3), 7))<CR>"; git push
+nmap <F9> :!cd ~/hacking/jekyll; git add '<C-R>=strftime('%F')<CR>-<C-R>=tolower(substitute(strpart(getline(3), 7), " ", "_", "g"))<CR>.mkd'; git ci -m '+ <C-R>=tolower(strpart(getline(3), 7))<CR>'; git push
 
 nmap <F12> :!rdiscount % > %.html; x-www-browser %.html<CR>
 
