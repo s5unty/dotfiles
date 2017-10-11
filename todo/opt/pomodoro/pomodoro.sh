160d67830a8b1a6024e8f88d34e678f2a0be15a7
Vern Sun <s5unty@gmail.com>
2017-10-11 10:44:52 +0800

    1507689892

--- todo/opt/pomodoro/pomodoro.sh
+++ todo/opt/pomodoro/pomodoro.sh
@@ -5,6 +5,8 @@
 #
 # with awesome-client, you need do something in your rc.lua:
 #
+#       [awesome-3.5.x]
+#       ----
 #       ...
 #       require("awful.remote") -- make `awesome-client` work
 #       ...
@@ -20,6 +22,32 @@
 #           mytaglist[s],
 #           pomodoro.widget,    -- right here
 #       ...
+#       ----
+#
+#       [awesome-4.x]
+#       ----
+#       ...
+#       require("awful.remote") -- make `awesome-client` happy
+#       ...
+#       pomodoro = wibox.widget.progressbar {}
+#       pomodoro.forced_width     = 0
+#       pomodoro.max_value        = 100
+#       pomodoro.background_color = theme.bg_normal
+#       pomodoro.color            = { type = "linear", from = {0, 0}, to = {100, 0}, stops = { { 0, '#AECF96' }, { 0.25, "#88A175" }, { 1, "#FF5656" } } }
+#       pomodoro.ticks            = true -- false:平滑的整体，true:间隙的个体
+#       ...
+#       mywibox[s].widgets = {
+#           ...
+#           mytaglist[s],
+#           pomodoro,    -- right here
+#       ...
+#       ----
+#
+# zsh/bash alias:
+#
+#       alias td1="(${TODO_DIR}/opt/pomodoro/pomodoro.sh '' 1 &)"
+#       alias td2="(${TODO_DIR}/opt/pomodoro/pomodoro.sh '' 2 &)"
+#
 #
 clean_up() {
     todo.sh stop $(cat ${TODO_DIR}/var/latest.tid) > /dev/null 2>&1
@@ -28,9 +56,9 @@ clean_up() {
     # python $TODO_DIR/opt/punch/Punch.py out > /dev/null 2>&1
     #
     # 计时改由在线的 Toggl 服务实现
-    $TODO_DIR/opt/toggl/toggl.py stop > /dev/null 2>&1
+    # $TODO_DIR/opt/toggl/toggl.py stop > /dev/null 2>&1
 
-    echo "pomodoro:set_background_color(theme.bg_normal);pomodoro:set_value(0);pomodoro:set_width(1);" | awesome-client
+    echo "pomodoro:set_background_color(theme.bg_normal);pomodoro:set_value(0);pomodoro.forced_width = 0;" | awesome-client
 	exit 0
 }
 trap clean_up HUP INT TERM KILL
@@ -41,7 +69,7 @@ tdid=${1}
 turn=${2}
 
 for t in $(seq ${turn}); do
-    echo "pomodoro:set_background_color('#494B4F');pomodoro:set_width(100);" | awesome-client
+    echo "pomodoro:set_background_color('#494B4F');pomodoro.forced_width = 100;" | awesome-client
     # 工作时间开始
     for i in $(seq 100); do
         echo "pomodoro:set_value(${i})" | awesome-client
@@ -49,12 +77,12 @@ for t in $(seq ${turn}); do
     done
 
     # 工作时间结束
-    echo 'local naughty = require("naughty"); naughty.notify({ margin = 4, position = "bottom_left", timeout=300, text = "Time to stop work and take a little break."})' | awesome-client
+    echo "local naughty = require('naughty'); naughty.notify({ margin = 5, position = 'bottom_left', timeout=${rest}, text = 'Time to stop work and take a little rest.'})" | awesome-client
 
     # 休息时间开始
     for i in $(seq 100); do
         j=$((100-i+1))
-        echo "pomodoro:set_width(${j})" | awesome-client
+        echo "pomodoro.forced_width = ${j}" | awesome-client
         sleep $(echo "scale=3;${rest}/100" | bc)
     done
 done
