ff784094f102ffca584d43b4a81b03e4fbd02bb6
Vern Sun <s5unty@gmail.com>
2018-12-30 10:56:02 +0800

    1546138562

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -84,20 +84,20 @@ local altkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile.right,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.corner.sw,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.corner.ne,
+    awful.layout.suit.corner.se,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.fair,
+    awful.layout.suit.tile.bottom,
     --awful.layout.suit.magnifier,
-    --awful.layout.suit.tile.left,
+    --awful.layout.suit.tile.right,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
-    --awful.layout.suit.fair.horizontal,
+    --awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
     --awful.layout.suit.corner.nw,
-    --awful.layout.suit.corner.ne,
-    --awful.layout.suit.corner.se,
+    --awful.layout.suit.corner.sw,
 }
 -- }}}
 
